160518ff8a75a706981c4a6a29d7151f1edb3950
suren-hov <shovhannisyan2021@gmail.com>
2025-07-30 21:19:51 +0400

    Define Eloquent Models

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Room extends Model
 {
-    //
+    protected $fillable = ['name'];
+
+    public function roomAccesses(): HasMany
+    {
+        return $this->hasMany(RoomAccess::class);
+    }
 }
--- app/Models/RoomAccess.php
+++ app/Models/RoomAccess.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class RoomAccess extends Model
 {
-    //
+    protected $fillable = [
+        'employee_id',
+        'room_id',
+        'day_of_week',
+        'start_time',
+        'end_time',
+    ];
+
+    public function employee(): BelongsTo
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function room(): BelongsTo
+    {
+        return $this->belongsTo(Room::class);
+    }
 }
