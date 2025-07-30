21b46b0a45f03e815d99233fd94ae0e5bff7bf35
suren-hov <shovhannisyan2021@gmail.com>
2025-07-31 01:42:33 +0400

    Fixed route issues

--- app/Http/Controllers/RoomAccessController.php
+++ app/Http/Controllers/RoomAccessController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRoomAccessRequest;
-use App\Models\RoomAccess;
 use App\Services\RoomAccessService;
 
 class RoomAccessController extends Controller
@@ -14,7 +13,6 @@ class RoomAccessController extends Controller
 
     public function store(StoreRoomAccessRequest $request)
     {
-        $this->authorize('create', RoomAccess::class);
         $access = $this->service->assign($request->validated());
         return response()->json($access, 201);
     }
