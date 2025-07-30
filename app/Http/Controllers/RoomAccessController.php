6a5259220d12d0df751f563464c7bd2b6ee81eb6
suren-hov <shovhannisyan2021@gmail.com>
2025-07-30 21:40:16 +0400

    Added policy

--- app/Http/Controllers/RoomAccessController.php
+++ app/Http/Controllers/RoomAccessController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreRoomAccessRequest;
+use App\Models\RoomAccess;
 use App\Services\RoomAccessService;
 
 class RoomAccessController extends Controller
@@ -13,6 +14,7 @@ class RoomAccessController extends Controller
 
     public function store(StoreRoomAccessRequest $request)
     {
+        $this->authorize('create', RoomAccess::class);
         $access = $this->service->assign($request->validated());
         return response()->json($access, 201);
     }
---
+++ app/Policies/RoomAccessPolicy.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\RoomAccess;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class RoomAccessPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, RoomAccess $roomAccess): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, RoomAccess $roomAccess): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, RoomAccess $roomAccess): bool
+    {
+        return $user->id === $roomAccess->employee->user_id;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, RoomAccess $roomAccess): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, RoomAccess $roomAccess): bool
+    {
+        return false;
+    }
+}
